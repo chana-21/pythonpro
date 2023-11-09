513b9b0aa8595fd02b28b13d6f34a9b06af185fa
chana <ami159@cu.ac.kr>
2023-11-09 16:03:08 +0900

    .

--- chap8/prob3.py
+++ chap8/prob3.py
@@ -1,35 +1,68 @@
+import pickle
+import shelve
 import os
 
 
-def file_write(file_path):
-    f = open(file_path, "w")
-    lines = []
+def file_write_pickle(file_path):
+    f = open(file_path, "wb")
+    variety = ["sweet", "hot", "dill"]
+    shape = ["whole", "spear", "chip"]
+    brand = ["Claussen", "Heinz", "Vlassic"]
 
-    print("Input your string...")
-    while True:
-        enter = input('>> ')
-        if enter == "Q" or enter == "q":
-            break
+    pickle.dump(variety, f)
+    pickle.dump(shape, f)
+    pickle.dump(brand, f)
 
-        lines.append(enter + "\n")
-
-    f.writelines(lines)
     f.close()
-    print("Write process has been finished\n\n")
 
 
-def file_read(file_path):
-    f = open(file_path, "r")
-    print("Your inputs are below...")
-    print(f.read())
+def file_read_pickle(file_path):
+    f = open(file_path, "rb")
+
+    variety = pickle.load(f)
+    shape = pickle.load(f)
+    brand = pickle.load(f)
+
+    print(variety)
+    print(shape)
+    print(brand)
+
+    f.close()
+
+
+def file_write_shelve(file_path):
+    f = shelve.open(file_path)
+
+    f["variety"] = ["sweet", "hot", "dill"]
+    f["shape"] = ["whole", "spear", "chip"]
+    f["brand"] = ["Claussen", "Heinz", "Vlassic"]
+
+    f.sync()
+    f.close()
+
+
+def file_read_shelve(file_path):
+    f = shelve.open(file_path)
+
+    for key in f.keys():
+        print(key, "-", f[key])
+
+    f.close()
 
 
 if __name__ == '__main__':
     path = os.getcwd()
 
-    file_path = os.path.join(path, "write_and_read.txt")
+    shelve_file_path = os.path.join(path, "shelve")
+    pickle_file_path = os.path.join(path, "pickle")
 
-    file_write(file_path)
-    file_read(file_path)
+    print("Pickling lists.\n")
+    file_write_pickle(pickle_file_path)
+    print("Unpickling lists.")
+    file_read_pickle(pickle_file_path)
 
+    print("\nShelving lists.\n")
+    file_write_shelve(shelve_file_path)
+    print("Retrieving the lists from a shelved file:")
+    file_read_shelve(shelve_file_path)
 
